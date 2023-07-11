2f62af948bf8c38942d3af003d50e9a62ae0dd58
PrinceCAD Studiosgit config --global user.email <Prince.cadstudios.204@gmail.com>
2023-07-11 12:21:49 +0100

    Strings

--- intro.js
+++ intro.js
@@ -1,37 +1,44 @@
-console.log("Hello World!");
+// console.log("Hello World!");
 
-// This is a comment
+// // This is a comment
 
-/* 
-This is a multi-line comment
-this is a second line
-and a third
-*/
+// /* 
+// This is a multi-line comment
+// this is a second line
+// and a third
+// */
 
-// Declaring variables
+// // Declaring variables
 
-var myVariable = 1;
-let myVariableName = "joel"
-const num = 3.145
+// var myVariable = 1;
+// let myVariableName = "joel"
+// const num = 3.145
 
-console.log(myVariable, myVariableName, num)
+// console.log(myVariable, myVariableName, num)
 
 
-console.log(typeof(initialValue))
-// Operations in js
-var a = 2, b = 5;
+// console.log(typeof(initialValue))
+// // Operations in js
+// var a = 2, b = 5;
 
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(b%a)
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(b%a)
 
 // Increaments and decreaments in Js
 
-var increamemt = 2, decreament = 7;
-console.log("\n")
-console.log(increamemt, decreament)
-increamemt+=1
-decreament-=1
-console.log(increamemt, decreament)
+// var increamemt = 2, decreament = 7;
+// console.log("\n")
+// console.log(increamemt, decreament)
+// increamemt+=1
+// decreament-=1
+// console.log(increamemt, decreament)
+
+
+
+// strings in js
+
+var initialValue = 'Hello, World!,\t Mr Nde told us "HEllo Guys" '
+console.log(initialValue)
