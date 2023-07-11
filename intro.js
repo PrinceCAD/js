7382cac87f6ac94781868ebb7641c4977f590863
PrinceCAD Studiosgit config --global user.email <Prince.cadstudios.204@gmail.com>
2023-07-11 11:24:14 +0100

    Initial commit

---
+++ intro.js
@@ -0,0 +1,37 @@
+console.log("Hello World!");
+
+// This is a comment
+
+/* 
+This is a multi-line comment
+this is a second line
+and a third
+*/
+
+// Declaring variables
+
+var myVariable = 15
+let myVariableName = "joel"
+const num = 3.145
+
+console.log(myVariable, myVariableName, num)
+
+
+console.log(typeof(initialValue))
+// Operations in js
+var a = 2, b = 5;
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(b%a)
+
+// Increaments and decreaments in Js
+
+var increamemt = 2, decreament = 7;
+console.log("\n")
+console.log(increamemt, decreament)
+increamemt+=1
+decreament-=1
+console.log(increamemt, decreament)
