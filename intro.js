4ef7475ed2c18a5917bd024e6fdb4720552d97e5
PrinceCAD Studiosgit config --global user.email <Prince.cadstudios.204@gmail.com>
2023-07-12 10:35:03 +0100

    Functions

--- intro.js
+++ intro.js
@@ -2,7 +2,7 @@
 
 // // This is a comment
 
-// /* 
+// /*
 // This is a multi-line comment
 // this is a second line
 // and a third
@@ -15,7 +15,6 @@
 // const num = 3.145
 
 // console.log(myVariable, myVariableName, num)
-
 
 // console.log(typeof(initialValue))
 // // Operations in js
@@ -36,9 +35,51 @@
 // decreament-=1
 // console.log(increamemt, decreament)
 
-
-
 // strings in js
 
-var initialValue = 'Hello, World!,\t Mr Nde told us "HEllo Guys" '
-console.log(initialValue)
+// var initialValue = 'Hello, World!, Mr Nde told us "HEllo Guys" ';
+// console.log(initialValue);
+
+// var string_1 = "Hello";
+// console.log(string_1 + " World!");
+
+// console.log(string_1[3]);
+// console.log(string_1.length);
+
+// // Arrays in JS
+// const myArray = [45, 23, "hello", null, true, ["yo", 25, 43]];
+
+// console.log(myArray);
+// myArray.push("joel");
+// console.log(myArray);
+// console.log(myArray.length);
+
+// console.log(myArray);
+// myArray.unshift("hey");
+// console.log(myArray);
+// console.log(myArray.length);
+
+// myArray.map((i, k) => {
+//   console.log(` Index: ${k} Value: ${i}`);
+// });
+// console.log(myArray.sort());
+// console.log(
+//   myArray.some((i) => {
+//     i = true;
+//   })
+// );
+
+
+// The common way of declaring functions in js
+// // function sumTwoNumbers(a ,b){
+//   return a+b;
+// }
+
+// console.log(sumTwoNumbers(2,5))
+
+// The ES6 syntax for functions
+// const sumTwoNumbers = (a,b) => {
+//   return a+b;
+// }
+
+// console.log(sumTwoNumbers(4,7))
